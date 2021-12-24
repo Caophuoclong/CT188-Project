c84b3731fba335a3bf5fbb8f53021b82c5feb720
Cakcak2001 <caophuoclong1@gmail.com>
2021-12-24 12:26:28 +0700

    update

--- assets/js/sales.js
+++ assets/js/sales.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    console.log(_10Sales);
-    console.log(_5Sales);
-
-    // renderSales(flashSales, ".flash-sales");
     renderProduct(_10Sales, ".10-sales", 1000000);
     renderProduct(_5Sales, ".5-sales", 1000000)
 })
